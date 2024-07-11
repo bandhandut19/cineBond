f70169672cfada3908684766afbeed79307b1fb8
bandhandut19 <bondon21081@gmail.com>
2024-07-11 18:23:35 +0600

    all movies get route done

--- src/components/TopMovies/TopMovies.tsx
+++ src/components/TopMovies/TopMovies.tsx
@@ -1,10 +1,31 @@
+import { useGetMoviesQuery } from "@/redux/api/api";
+import { MovieCard } from "../MovieCard/MovieCard";
+import { TMovie } from "../../types";
+
 const TopMovies = () => {
+  const { data, isLoading, isError } = useGetMoviesQuery({});
+  console.log(data);
+  if (isLoading) {
+    return <p>Loading....</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading movies. Please try again later.</p>;
+  }
+
+  const movies: TMovie[] | undefined = data?.data;
+
   return (
     <div className="my-5">
       <h1 className="text-4xl font-bold text-yellow-400">What to watch</h1>
-      <h2 className="text-2xl font-bold my-2  border-l-4 border-l-yellow-400 px-1">
+      <h2 className="text-2xl font-bold my-2 border-l-4 border-l-yellow-400 px-1">
         Top Rated Movies
       </h2>
+      <div className="grid grid-cols-4 gap-4">
+        {movies?.map((movie) => (
+          <MovieCard key={movie?._id} movie={movie}></MovieCard>
+        ))}
+      </div>
     </div>
   );
 };
